de3f915c1e5270b6bd2407fdd4a5f70af7572aee
achal1304 <achal20000@gmail.com>
2021-11-26 14:51:31 +0530

    Created basic structure including helpers, middlewares and errors.go

---
+++ cmd/api/middleware.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
+		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+		next.ServeHTTP(w, r)
+	})
+}
