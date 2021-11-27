498846a2ba4fa1e5e5a8024bb9aa9e95b006847c
gopheramit <amitwpu@gmail.com>
2021-11-27 15:24:33 +0530

    smtp

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,12 +4,13 @@ import "net/http"
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Vary", "Origin")
-		w.Header().Add("Vary", "Access-Control-Request-Method")
-
-		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE,POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
-
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		//w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		//w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Content-Type", "application/json")
+		//w.WriteHeader(http.StatusOK)
 		next.ServeHTTP(w, r)
 	})
 }
